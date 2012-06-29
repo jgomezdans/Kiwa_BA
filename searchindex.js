a1cc9e0af2dc462179514a279a4ca98e869f6d56
Jose Gómez-Dans <j.gomez-dans@ucl.ac.uk>
2012-06-29 13:11:39 +0100

    First commit post fabric script

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,project:0,area:0,kiwa:0,modul:0,burn:0,indic:0,content:0,tabl:0,document:0,page:0},objtypes:{},titles:["Welcome to Kiwa burned area project &#8216;s documentation!"],objnames:{},filenames:["index"]})
